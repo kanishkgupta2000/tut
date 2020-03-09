5bd74e5f3292deb594e149158ff9d3071cf02037
Kanishk <kanishkgupta2000@gmail.com>
2020-03-09 21:30:42 +0530

    socket setup

---
+++ index.js
@@ -0,0 +1,16 @@
+var express=require("express"); 
+var socket=require('socket.io');
+var app=express();
+var server= app.listen(4000,()=>{
+    console.log("listen to port 4000");
+})
+
+app.use(express.static('public'));
+
+//socket setup
+
+var io=socket(server);
+
+io.on('connection',(socket)=>{
+    console.log('made socket connection')
+})
