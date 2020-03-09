612cb3576f74e28118b5801847f5dfade655db42
Kanishk <kanishkgupta2000@gmail.com>
2020-03-09 22:33:03 +0530

    working

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ var io=socket(server);
 
 io.on('connection',(socket)=>{
     console.log('made socket connection',socket.id)
+
+    socket.on('chat',(data)=>{
+        io.sockets.emit('chat',data)
+    })
 })
