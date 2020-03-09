5a6cc72a16845c69d8ae9539ee6275a555e5bd98
Kanishk <kanishkgupta2000@gmail.com>
2020-03-09 21:37:45 +0530

    add interface

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.use(express.static('public'));
 var io=socket(server);
 
 io.on('connection',(socket)=>{
-    console.log('made socket connection')
+    console.log('made socket connection',socket.id)
 })
